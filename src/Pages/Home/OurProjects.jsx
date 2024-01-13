dbd5783288e106d838ad4b9474dead2a311c5c50
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2024-01-14 02:37:39 +0530

    add some

--- src/Pages/Home/OurProjects.jsx
+++ src/Pages/Home/OurProjects.jsx
@@ -1,34 +1,45 @@
 import { BiSolidRightArrow } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
+import image1 from '../../assets/ruthvi.webp';
+import image2 from '../../assets/destiny.webp';
+import image3 from '../../assets/jyesta.webp';
+import image4 from '../../assets/atharva.webp';
+import image5 from '../../assets/lotus.webp';
+
 const OurProjects = () => {
     return (
         <section className="py-10 md:px-20 px-4">
             <h2 className="md:text-5xl text-3xl md:mb-10 mb-5 md:text-right">OUR <span className='text-slate-400'>PROJECTS</span></h2>
-            <div className="grid md:grid-cols-3 grid-cols-1 md:grid-rows-4 gap-5">
-                <div className="bg-slate-200 p-4 min-h-48 relative md:col-span-2">
-                    <div className="absolute bottom-3 right-4">
+            <div className="grid md:grid-cols-3 grid-cols-1 md:grid-rows-4 gap-5 md:text-base text-xs">
+                <div className="bg-slate-200 min-h-48 relative md:col-span-2 overflow-hidden">
+                    <img src={image1} alt="" className="w-full h-full hover:scale-105 duration-200" />
+                    <div className="absolute md:bottom-3 md:right-4 bottom-1 right-1">
                         <Link className="flex items-center gap-1 hover:outline outline-1 hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
                     </div>
                 </div>
-                <div className="bg-slate-200 p-4 min-h-48 relative row-span-4">
-                    <div className="absolute bottom-3 right-4">
-                        <Link className="flex items-center gap-1 hover:outline outline-1 hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
+                <div className="bg-slate-200 min-h-48 relative row-span-4 overflow-hidden">
+                    <img src={image2} alt="" className="w-full h-full hover:scale-105 duration-200" />
+                    <div className="absolute md:bottom-3 md:right-4 bottom-1 right-1">
+                        <Link className="flex items-center gap-1 hover:outline outline-1 text-white hover:text-black hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
                     </div>
                 </div>
-                <div className="bg-slate-200 p-4 min-h-48 relative row-span-2">
-                    <div className="absolute bottom-3 right-4">
-                        <Link className="flex items-center gap-1 hover:outline outline-1 hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
+                <div className="bg-slate-200 min-h-48 relative row-span-2 overflow-hidden">
+                    <img src={image3} alt="" className="w-full h-full hover:scale-105 duration-200" />
+                    <div className="absolute md:bottom-3 md:right-4 bottom-1 right-1">
+                        <Link className="flex items-center gap-1 hover:outline outline-1 text-white hover:text-black hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-300" /></Link>
                     </div>
                 </div>
-                <div className="bg-slate-200 p-4 min-h-48 relative row-span-3">
-                    <div className="absolute bottom-3 right-4">
-                        <Link className="flex items-center gap-1 hover:outline outline-1 hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
+                <div className="bg-slate-200 min-h-48 relative row-span-3 overflow-hidden">
+                    <img src={image4} alt="" className="w-full h-full hover:scale-105 duration-200" />
+                    <div className="absolute md:bottom-3 md:right-4 bottom-1 right-1">
+                        <Link className="flex items-center gap-1 hover:outline outline-1 text-white hover:text-black hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
                     </div>
                 </div>
-                <div className="bg-slate-200 p-4 min-h-48 relative">
-                    <div className="absolute bottom-3 right-4">
-                        <Link className="flex items-center gap-1 hover:outline outline-1 hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-500" /></Link>
+                <div className="bg-slate-200 min-h-48 relative overflow-hidden">
+                    <img src={image5} alt="" className="w-full h-full hover:scale-105 duration-200" />
+                    <div className="absolute md:bottom-3 md:right-4 bottom-1 right-1">
+                        <Link className="flex items-center gap-1 hover:outline outline-1 text-white hover:text-black hover:bg-white px-2 py-1">EXPLORE <BiSolidRightArrow className="text-slate-300" /></Link>
                     </div>
                 </div>
             </div>
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,4 +1,6 @@
-import image from '../../assets/projects.jpg';
+import coverBg from '../../assets/project-back-desktop.webp';
+
+import logo from '../../assets/logoash.webp';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -8,24 +10,27 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Pagination, Autoplay } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 
 const Projects = () => {
     return (
         <section className="">
-            <div className='md:h-96 h-40 bg-cover bg-center' style={{ backgroundImage: `url(${image})` }} >
-                <div className='h-full grid place-content-center backdrop-brightness-[.25]'>
-                    <h1 className='md:text-5xl text-2xl uppercase font-bold text-white'>Our Projects</h1>
+            <div className='md:h-[28rem] h-96 bg-cover bg-center' style={{ backgroundImage: `url(${coverBg})` }} >
+                <div className='h-full backdrop-brightness-50 relative'>
+                    <img src={logo} alt="" className='w-20 md:mx-0 mx-auto absolute top-5 md:left-20 left-5' />
+                    <h2 className='text-white md:text-6xl text-xl absolute bottom-20 left-1/2 -translate-x-1/2' style={{ fontFamily: "Herr Von Muellerhoff" }} >where plots transform into destinies</h2>
+                    <h1 className='md:text-5xl text-2xl uppercase font-bold text-white absolute right-5 bottom-5'>Projects</h1>
                 </div>
             </div>
 
             <div className='md:my-20 my-10 md:px-20 px-4'>
 
-                <div className='flex md:gap-8 gap-2 flex-wrap md:text-base text-sm'>
-                    <button className='px-4 py-2 bg-slate-300 hover:bg-slate-500 hover:text-white' >All Projects</button>
-                    <button className='px-4 py-2 bg-slate-300 hover:bg-slate-500 hover:text-white' >Ongoing Projects</button>
-                    <button className='px-4 py-2 bg-slate-300 hover:bg-slate-500 hover:text-white' >Complete Projects</button>
-                    <button className='px-4 py-2 bg-slate-300 hover:bg-slate-500 hover:text-white' >Upcoming Projects</button>
+                <div className='flex md:gap-8 gap-2 md:text-sm text-xs justify-center'>
+                    {/* <button className='px-4 py-2 border-b border-black hover:bg-slate-300' >All Projects</button> */}
+                    <button className='md:px-4 px-2 py-2 border-b border-black hover:bg-slate-300' >ONGOING PROJECTS</button>
+                    <button className='md:px-4 px-2 py-2 border-b border-black hover:bg-slate-300' >COMPLETE PROJECTS</button>
+                    <button className='md:px-4 px-2 py-2 border-b border-black hover:bg-slate-300' >UPCOMING PROJECTS</button>
                 </div>
                 <div className='pt-10'>
                     <Swiper
@@ -62,9 +67,16 @@ const Projects = () => {
                             [...Array(7)].map((_, i) => {
 
                                 return <SwiperSlide key={i}>
-                                    <div className="h-80 bg-[url(https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661456.jpg)] bg-cover bg-center">
-                                        <div className='backdrop-brightness-50 h-full text-white grid place-content-center text-xl uppercase font-semibold'>
-                                            <h1>New Project</h1>
+                                    <div className="bg-cover bg-center">
+                                        <img src="https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661456.jpg" alt="" className='h-72 w-full object-cover' />
+                                        <div className='text-center font-bold py-5'>
+                                            <h5 className='text-sm'>ASHWASURYA</h5>
+                                            <h2 className='text-xl'>DESTINY FARM</h2>
+                                            <div className='mt-2'>
+                                                <Link className='text-sm font-medium px-2 py-1 border border-black duration-200 hover:bg-black hover:text-white'>
+                                                    KNOW MORE
+                                                </Link>
+                                            </div>
                                         </div>
                                     </div>
                                 </SwiperSlide>
